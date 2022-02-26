9416a4da02953ca612085ad3a93e0c26db97be45
Vsevolod Stakhov <vsevolod@highsecure.ru>
2022-02-26 16:59:18 +0000

    Be more tolerate for network errors

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ use std::{
 	time::Duration,
 };
 
+const MAX_NET_ERRORS: i32 = 5;
+
 #[derive(Debug, Parser)]
 pub(crate) struct CliOpts {
 	/// Chart width.
@@ -309,26 +311,29 @@ async fn main() -> color_eyre::Result<()> {
 	tokio::spawn(async move {
 		let stats = stats.clone();
 		let mut niter = 0;
+		let mut error_counter = 0;
+		let mut elapsed = Duration::from_secs_f32(opts.timeout);
 		loop {
-			let _ = stdout().queue(Clear(ClearType::All)).unwrap();
 			let timeout = Duration::from_secs_f32(opts.timeout);
 			let client = reqwest::Client::builder().timeout(timeout).user_agent("rspamd-mon").build()?;
 			let req = client.get(opts.url.as_str()).send();
 			let resp = match req.await {
 				Ok(o) => o.bytes(),
 				Err(e) => {
+					// We should be able to send request
 					return Err(eyre!("cannot get send request to {}: {}", opts.url.as_str(), e));
 				},
 			};
-			match resp.await {
+			let res = match resp.await {
 				Ok(o) => {
 					let json: serde_json::Value = serde_json::from_slice(&o)
 						.map_err(|e| eyre!("malformed json from {}: {}", opts.url.as_str(), e))?;
 					let mut stats_unlocked = stats.lock().await;
 					stats_unlocked
-						.update_from_json(json, timeout)
+						.update_from_json(json, elapsed)
 						.map_err(|e| eyre!("cannot get results from {}: {}", opts.url.as_str(), e))?;
 					if niter > 0 {
+						let _ = stdout().queue(Clear(ClearType::All)).unwrap();
 						stats_unlocked.display_chart(opts.chart_height as u16);
 					}
 					niter += 1;
@@ -336,7 +341,23 @@ async fn main() -> color_eyre::Result<()> {
 					Ok(())
 				},
 				Err(e) => Err(eyre!("cannot get results from {}: {}", opts.url.as_str(), e)),
+			};
+
+			let _ = if let Err(e) = res {
+				error_counter += 1;
+				elapsed = elapsed + elapsed;
+
+				if error_counter > MAX_NET_ERRORS {
+					Err(e)
+				} else {
+					Ok(())
+				}
+			} else {
+				error_counter = 0;
+				elapsed = timeout;
+				Ok(())
 			}?;
+
 			let _ = stdout().flush();
 			tokio::time::sleep(timeout).await;
 		}
